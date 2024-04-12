39a16e814174d9b6f0ae569c4c40ee78a8c4de16
Lazy-D-Bit <161049991+Lazy-D-Bit@users.noreply.github.com>
2024-04-12 11:45:51 +0530

    Added button and Promt

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-const container = document.getElementById("container")
-container.style.display = "flex";
-container.style.flexDirection = "column";
-for (let j = 0; j < 16; j++) {
-    const row = document.createElement("div");
-    container.appendChild(row);
-    row.style.display = "flex"
-    for (let i = 0; i < 16; i++) {
-        const square = document.createElement("div");
-        square.style.width = "25px";
-        square.style.height = "25px";
-        square.style.backgroundColor = "#eeeeee";
-        square.style.border="2px solid black";
-        row.appendChild(square);
-        square.addEventListener("mouseover", function() {
-            square.style.backgroundColor = "#3d85c6";
-        });
+canvas(10);
+
+const button = document.getElementById("num");
+button.addEventListener("click", function() {
+    const num = prompt("Enter GRID size (size limit: 0 < size <= 100):");
+
+    deleteCanvas();
+    canvas(num);
+})
+function canvas(num) {
+    const container = document.getElementById("container")
+    container.style.display = "flex";
+    container.style.flexDirection = "column";
+    container.style.width = "600px"
+    container.style.height = "600px"
+    for (let j = 0; j < num; j++) {
+        const row = document.createElement("div");
+        container.appendChild(row);
+        row.style.display = "flex";
+        row.style.flex = "1";
+        for (let i = 0; i < num; i++) {
+            const square = document.createElement("div");
+            square.style.backgroundColor = "#eeeeee";
+            square.style.border="1px solid black";
+            square.style.flex = "1";
+            square.style.margin = "0";
+            row.appendChild(square);
+            square.addEventListener("mouseover", function() {
+                square.style.backgroundColor = "#3d85c6";
+            });
+        }
     }
 }
-
+function deleteCanvas() {
+    const container = document.getElementById("container");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
